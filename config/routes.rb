80f919c6a411358db5b5cdfe29aa1e2b2fbbea26
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-04-02 17:44:41 +0900

    I5 done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  
   resources :tags
+ 
   root to: 'articles#index'
+  
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
